a4e704be38ed8e9f555e87deb400abf8b3d7324a
OwenVm <owen.vermeulen@kuleuven.be>
2026-05-01 21:56:25 +0200

    change sign

--- src/main/inject_wind_pulsating.f90
+++ src/main/inject_wind_pulsating.f90
@@ -227,8 +227,8 @@ subroutine init_inject(ierr)
  piston_velocity  = piston_velocity_km_s * (km / unit_velocity)
  deltaR_osc       = pulsation_period * piston_velocity / (2.0*pi)
 
- r_min          = r_min_on_rstar * Rstar - deltaR_osc * sin(phi0)
- r_max_on_rstar = r_max_on_rstar - (deltaR_osc * sin(phi0)) / Rstar
+ r_min          = r_min_on_rstar * Rstar + deltaR_osc * sin(phi0)
+ r_max_on_rstar = r_max_on_rstar + (deltaR_osc * sin(phi0)) / Rstar
 
  if (save_period) then
     dtmax = 1. / (dumps_p_period) * pulsation_period
