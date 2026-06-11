244aaca5ed8529e926ac88498e9e0fb0fd64cbc8
OwenVm <owen.vermeulen@kuleuven.be>
2026-06-11 10:45:44 +1000

    random push

--- src/main/utils_inject.f90
+++ src/main/utils_inject.f90
@@ -205,13 +205,13 @@ subroutine inject_fibonacci_sphere(sphere_number, first_particle, n_particles, r
  
  ! Rotation angles to vary sphere orientation based on sphere number
  ! (helps prevent alignment between consecutive spheres)
- rotation_angles = (/ 1.28693610288783, 2.97863087745917, 1.03952835451832 /) * sphere_number
+ rotation_angles = (/ 2.90443293496604, 1.77939686318657, 1.04113050588920 /) * sphere_number
+
  call make_rotation_matrix(rotation_angles, rotmat)
  
  ! Smoothing length in simulation units
  h_sim = hrho(rho)
 
- ! Place particles using Fibonacci lattice
  do i = 0, n_particles-1
     i_float = real(i)
     
@@ -240,7 +240,6 @@ subroutine inject_fibonacci_sphere(sphere_number, first_particle, n_particles, r
                                   + radial_unit_vector(2)*rotmat(3,2) &
                                   + radial_unit_vector(3)*rotmat(3,3)
     
-    ! Scale to desired radius and add center position/velocity
     particle_position = r * radial_unit_vector_rotated + x0
     particle_velocity = v * radial_unit_vector_rotated + v0
     
@@ -285,5 +284,38 @@ subroutine make_rotation_matrix(rotation_angles, rot_m)
  rot_m(3,3) = c_x*c_y
 
 end subroutine make_rotation_matrix
+
+subroutine find_optimal_rotation_matrix(n_particles, rotation_angles)
+   ! This routine is a placeholder for future implementation of an optimization algorithm
+   ! to find the best rotation matrix that minimizes particle clustering on the sphere.
+   ! For now, it simply returns an identity matrix.
+   real, intent(in) :: n_particles
+   real, intent(out) ::  rotation_angles(3)
+   real :: optimal_rot_m(3, 3), rot_m_ini(3, 3), optimal_rotation_angles(3)
+   real :: init_positions(3, :), rotated_positions(3, :), distances(:)
+   real :: minpos, maxpos, avgdist
+   integer :: i, j
+   
+   ! Initialize to identity matrix
+   do i = 1, 3
+      do j = 1, 3
+         if (i == j) then
+            rot_m_ini(i,j) = 1.0
+         else
+            rot_m_ini(i,j) = 0.0
+         endif
+      enddo
+   enddo
+   
+   rotation_angles = (/ 0.0, 0.0, 0.0 /) ! Identity rotation
+   
+ end subroutine find_optimal_rotation_matrix
+
+
+ subroutine distance_between_particles(pos1, pos2, distance)
+    real, intent(in) :: pos1(3), pos2(3)
+    real, intent(out) :: distance
+    distance = sqrt((pos1(1) - pos2(1))**2 + (pos1(2) - pos2(2))**2 + (pos1(3) - pos2(3))**2)
+ end subroutine distance_between_particles
 
 end module injectutils
